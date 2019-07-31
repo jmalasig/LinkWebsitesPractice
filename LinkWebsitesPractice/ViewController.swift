1d9e19c858ff35e876af3b30cfdbf197521e74b5
jmalasig <joanamalasig@gmail.com>
2019-07-31 15:17:30 -0400

    Initial Commit

--- LinkWebsitesPractice/ViewController.swift
+++ LinkWebsitesPractice/ViewController.swift
@@ -15,6 +15,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    //opens Facebook on Safari
+    @IBAction func FacebookTapped(_ sender: Any) {
+        UIApplication.shared.open(URL(string:"https://www.facebook.com")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    //opens Youtube on Safari
+    @IBAction func YoutubeTapped(_ sender: Any) {//name of variable shows up as back button
+        UIApplication.shared.open(URL(string:"https://www.youtube.com")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func backFromGoogle(_ sender: Any) {
+        UIApplication.shared.open(URL(string:"https://www.google.com")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    
+    @IBAction func DemoVideoTapped(_ sender: Any) {
+        UIApplication.shared.open(URL(string:"https://www.youtube.com/watch?v=75p-N9YKqNo")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
 }
 
